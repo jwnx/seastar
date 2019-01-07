bhalevy/seastar-old

reactor: add file_accessible method
Needed for checking file access permissions to determine if it's
linkable under sysctl fs.protected_hardlinks.

Refs scylladb/scylla#3117

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

include/seastar/core/reactor.hh
@@ -540,6 +540,21 @@ inline open_flags operator&(open_flags a, open_flags b) {
     return open_flags(std::underlying_type_t<open_flags>(a) & std::underlying_type_t<open_flags>(b));
 }
 
+// Access flags for files/directories
+enum class access_flags {
+    exists = F_OK,
+    read = R_OK,
+    write = W_OK,
+    execute = X_OK,
+
+    // alias for directory access
+    lookup = execute,
+};
+
+inline access_flags operator|(access_flags a, access_flags b) {
+    return access_flags(std::underlying_type_t<access_flags>(a) | std::underlying_type_t<access_flags>(b));
+}
+
 class io_queue {
 private:
     struct priority_class_data {
@@ -649,6 +664,7 @@ class disk_config_params;
 
 class reactor {
     using sched_clock = std::chrono::steady_clock;
+    using path = compat::filesystem::path;
 private:
     struct pollfn {
         virtual ~pollfn() {}
@@ -985,6 +1001,10 @@ public:
     future<> touch_directory(sstring name);
     future<compat::optional<directory_entry_type>>  file_type(sstring name);
     future<uint64_t> file_size(sstring pathname);
+    future<bool> file_accessible(path path, access_flags flags);
+    future<bool> file_accessible(sstring pathname, access_flags flags) {
+        return file_accessible(path(pathname), flags);
+    }
     future<bool> file_exists(sstring pathname);
     future<fs_type> file_system_at(sstring pathname);
     future<struct statvfs> statvfs(sstring pathname);

src/core/reactor.cc
@@ -2165,6 +2165,38 @@ reactor::file_size(sstring pathname) {
 }
 
 future<bool>
+reactor::file_accessible(fs::path path, access_flags flags) {
+    return _thread_pool.submit<syscall_result<int>>([path, flags] {
+        auto native_flags = std::underlying_type_t<access_flags>(flags);
+        auto ret = ::access(path.native().c_str(), native_flags);
+        return wrap_syscall(ret);
+    }).then([this, path, flags] (syscall_result<int> sr) {
+        if (sr.result < 0) {
+            if (sr.error == ENOENT && flags == access_flags::exists) {
+                return make_ready_future<bool>(false);
+            }
+            // The requested access would be denied to the file, or search permission is denied
+            // for one of the directories in the path prefix of pathname.
+            if (sr.error == EACCES && flags != access_flags::exists) {
+                auto parent = std::move(path);
+                parent.remove_filename();
+                return _thread_pool.submit<syscall_result<int>>([parent] {
+                    auto ret = ::access(parent.native().c_str(), X_OK);
+                    return wrap_syscall(ret);
+                }).then([path] (syscall_result<int> psr) {
+                    psr.throw_fs_exception_if_error("lookup failed", path);
+                    // only last path component can't be accessed
+                    return make_ready_future<bool>(false);
+                });
+            }
+            sr.throw_fs_exception("access failed", path);
+        }
+
+        return make_ready_future<bool>(true);
+    });
+}
+
+future<bool>
 reactor::file_exists(sstring pathname) {
     return _thread_pool.submit<syscall_result_extra<struct stat>>([pathname] {
         struct stat st;
@@ -4839,6 +4871,10 @@ future<uint64_t> fs_free(sstring name) {
 
 future<uint64_t> file_size(sstring name) {
     return engine().file_size(name);
+}
+
+future<bool> file_accessible(sstring name, access_flags flags) {
+    return engine().file_accessible(name, flags);
 }
 
 future<bool> file_exists(sstring name) {

jwnx/seastar

include/seastar/core/reactor.hh
@@ -451,6 +451,21 @@ inline open_flags operator&(open_flags a, open_flags b) {
     return open_flags(std::underlying_type_t<open_flags>(a) & std::underlying_type_t<open_flags>(b));
 }
 
+// Access flags for files/directories
+enum class access_flags {
+    exists = F_OK,
+    read = R_OK,
+    write = W_OK,
+    execute = X_OK,
+
+    // alias for directory access
+    lookup = execute,
+};
+
+inline access_flags operator|(access_flags a, access_flags b) {
+    return access_flags(std::underlying_type_t<access_flags>(a) | std::underlying_type_t<access_flags>(b));
+}
+
 class reactor_backend;
 
 class io_queue {
@@ -900,6 +915,7 @@ public:
     future<> touch_directory(sstring name);
     future<compat::optional<directory_entry_type>>  file_type(sstring name);
     future<uint64_t> file_size(sstring pathname);
+    future<bool> file_accessible(sstring pathname, access_flags flags);
     future<bool> file_exists(sstring pathname);
     future<fs_type> file_system_at(sstring pathname);
     future<struct statvfs> statvfs(sstring pathname);

src/core/reactor.cc
@@ -2649,6 +2649,25 @@ reactor::file_size(sstring pathname) {
 }
 
 future<bool>
+reactor::file_accessible(sstring pathname, access_flags flags) {
+    return _thread_pool.submit<syscall_result<int>>([pathname, flags] {
+        auto aflags = std::underlying_type_t<access_flags>(flags);
+        auto ret = ::access(pathname.c_str(), aflags);
+        return wrap_syscall(ret);
+    }).then([this, pathname, flags] (syscall_result<int> sr) {
+        if (sr.result < 0) {
+            if ((sr.error == ENOENT && flags == access_flags::exists) ||
+                (sr.error == EACCES && flags != access_flags::exists)) {
+                return make_ready_future<bool>(false);
+            }
+            sr.throw_fs_exception("access failed", fs::path(pathname));
+        }
+
+        return make_ready_future<bool>(true);
+    });
+}
+
+future<bool>
 reactor::file_exists(sstring pathname) {
     return _thread_pool.submit<syscall_result_extra<struct stat>>([pathname] {
         struct stat st;
@@ -5251,6 +5270,10 @@ future<uint64_t> fs_free(sstring name) {
 
 future<uint64_t> file_size(sstring name) {
     return engine().file_size(name);
+}
+
+future<bool> file_accessible(sstring name, access_flags flags) {
+    return engine().file_accessible(name, flags);
 }
 
 future<bool> file_exists(sstring name) {